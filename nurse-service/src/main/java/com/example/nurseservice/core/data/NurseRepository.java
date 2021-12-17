9138eed1430204accd96ea5813cb14af1c801061
it62070040 <56312650+it62070040@users.noreply.github.com>
2021-12-17 19:26:02 +0700

    Delete nurse-service directory

--- nurse-service/src/main/java/com/example/nurseservice/core/data/NurseRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.nurseservice.core.data;
-
-
-import com.example.nurseservice.core.NurseEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface NurseRepository extends JpaRepository<NurseEntity, String>{
-    NurseEntity findByNurseId(String nurseId);
-}
